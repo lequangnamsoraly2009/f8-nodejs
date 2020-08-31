34f71fc499feaa5e8c730fc5a70708cdf965004c
lequangnamsoraly2009 <lequangnam1617@gmail.com>
2020-09-01 03:40:23 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const port = 3000
 app.use(morgan('combined'))
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 // Template Engines
 app.engine('hbs', handlebars({
@@ -28,6 +32,10 @@ app.get('/', (req, res) => {
 
 app.get('/game', (req, res) => {
     res.render('game')
+})
+
+app.get('/search', (req, res) => {
+    res.render('search')
 })
 
 app.listen(port, () => {
